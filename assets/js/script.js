e396749f69c33acbb50d539429e3d31a6b00a155
HBica05 <bicahaadiyah@gmail.com>
2024-07-02 19:38:35 +0200

    Add an event preventDefault function to prevent form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,12 @@ const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 const special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?', '|', '~'];
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+/**
+ * docstring...
+ * @returns 
+ */
+function validateForm(event) {
+    // Prevent form submission
+    event.preventDefault();
+}

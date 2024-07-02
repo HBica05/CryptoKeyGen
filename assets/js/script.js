ec298449b08aad9e81465df237dd3cf0b0812fb6
HBica05 <bicahaadiyah@gmail.com>
2024-07-02 19:43:54 +0200

    Retrieve form input values and checkbox states

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-const special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?', '|', '~'];
+const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?', '|', '~'];
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 /**
@@ -11,3 +11,8 @@ function validateForm(event) {
     // Prevent form submission
     event.preventDefault();
 }
+// Get form values
+const length = document.getElementById('length').value;
+specialChars = document.getElementById('specialChars').checked;
+capitalLetters = document.getElementById('capitalLetters').checked;
+numbers = document.getElementById('numbers').checked;

8ae49ba874c7450b0b9ffd067ea075689d79ccd5
HBica05 <bicahaadiyah@gmail.com>
2024-07-02 19:34:47 +0200

    Declare array of lower and upper-case letters, special characters and numbers

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?', '|', '~'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
